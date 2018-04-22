94168d889bf7e113c9fca4c8bd1be0a129054f5b
Shawn Rakowski <srakowski@live.com>
2018-04-21 19:07:40 -0500

    some initial prototyping

---
+++ LD41.Raycasting/Raycaster.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace LD41.Raycasting
+{
+    static class Raycaster
+    {
+        public static Color[] Raycast(
+            int[,] mapGrid,
+            Point screenDim,
+            Vector2 position,
+            Vector2 direction,
+            Vector2 plane,
+            Texture2D[] textures)
+        {
+            var buffer = new Color[screenDim.Y, screenDim.X];
+            var textureData = new Color[textures.Length][];
+            int i = 0;
+            foreach (var texture in textures)
+            {
+                textureData[i] = new Color[texture.Width * texture.Height];
+                texture.GetData(textureData[i]);
+                i++;
+            }
+
+            for (int x = 0; x < screenDim.X; x++)
+            {
+                var cameraX = (2 * (x / (float)screenDim.X)) - 1; // where on the plane -1 to 1
+
+                var rayDirection = direction + (plane * cameraX);
+
+                var mapPos = position.ToPoint();
+
+                var sideDist = Vector2.Zero;
+
+                var deltaDist = Vector2.One;
+                deltaDist.X /= Math.Abs(rayDirection.X);
+                deltaDist.Y /= Math.Abs(rayDirection.Y);
+
+                var step = Point.Zero;
+
+                step.X = rayDirection.X < 0 ? -1 : 1;
+                sideDist.X = rayDirection.X < 0
+                    ? (position.X - mapPos.X) * deltaDist.X
+                    : (mapPos.X + 1f - position.X) * deltaDist.X;
+
+                step.Y = rayDirection.Y < 0 ? -1 : 1;
+                sideDist.Y = rayDirection.Y < 0
+                    ? (position.Y - mapPos.Y) * deltaDist.Y
+                    : (mapPos.Y + 1f - position.Y) * deltaDist.Y;
+
+                var yWallHit = false;
+                while (true)
+                {
+                    if (sideDist.X < sideDist.Y)
+                    {
+                        sideDist.X += deltaDist.X;
+                        mapPos.X += step.X;
+                        yWallHit = false;
+                    }
+                    else
+                    {
+                        sideDist.Y += deltaDist.Y;
+                        mapPos.Y += step.Y;
+                        yWallHit = true;
+                    }
+
+                    if (mapGrid[mapPos.X, mapPos.Y] > 0)
+                        break;
+                }
+
+                var perpWallDist = 0.0;
+                if (!yWallHit) perpWallDist = (mapPos.X - position.X + (1 - step.X) / 2) / rayDirection.X;
+                else perpWallDist = (mapPos.Y - position.Y + (1 - step.Y) / 2) / rayDirection.Y;
+
+                var lineHeight = (int)(screenDim.Y / perpWallDist);
+
+                var halfLineHeight = lineHeight / 2;
+                var midScreen = screenDim.Y / 2;
+                var drawStart = MathHelper.Clamp(midScreen - halfLineHeight, 0, screenDim.Y);
+                var drawEnd = MathHelper.Clamp(midScreen + halfLineHeight, 0, screenDim.Y);
+
+                var texIdx = mapGrid[mapPos.X, mapPos.Y] - 1;
+                var tex = textures[texIdx];
+                var data = textureData[texIdx];
+
+                double wallX = 0;
+                if (!yWallHit) wallX = position.Y + (perpWallDist * rayDirection.Y);
+                else wallX = position.X + (perpWallDist * rayDirection.X);
+                wallX -= Math.Floor(wallX);
+
+                int texX = (int)(wallX * tex.Width);
+                if ((!yWallHit && rayDirection.X > 0) || (yWallHit && rayDirection.Y < 0))
+                    texX = tex.Width - texX - 1;
+
+                var light = (float)(Light(perpWallDist));
+
+                for (int y = drawStart; y < drawEnd + 1; y++)
+                {
+                    int d = (y * 256) - (screenDim.Y * 128) + (lineHeight * 128);
+                    int texY = ((d * tex.Height) / lineHeight) / 256;
+                    var dataIdx = MathHelper.Clamp((tex.Width * texY) + texX, 0, data.Length - 1);
+                    if (y < screenDim.Y)
+                    buffer[y, x] = new Color(data[dataIdx], light);
+                }
+
+                var floorXWall = 0.0;
+                var floorYWall = 0.0;
+
+                if (!yWallHit && rayDirection.X > 0)
+                {
+                    floorXWall = mapPos.X;
+                    floorYWall = mapPos.Y + wallX;
+                }
+                else if (!yWallHit && rayDirection.X < 0)
+                {
+                    floorXWall = mapPos.X + 1.0;
+                    floorYWall = mapPos.Y + wallX;
+                }
+                else if (yWallHit && rayDirection.Y > 0)
+                {
+                    floorXWall = mapPos.X + wallX;
+                    floorYWall = mapPos.Y;
+                }
+                else
+                {
+                    floorXWall = mapPos.X + wallX;
+                    floorYWall = mapPos.Y + 1.0;
+                }
+
+                var distWall = 0.0;
+                var distPlayer = 0.0;
+                var currentDist = 0.0;
+
+                distWall = perpWallDist;
+                distPlayer = 0.0;
+
+                if (drawEnd < 0) drawEnd = screenDim.Y;
+
+                tex = textures[0];
+                data = textureData[0];
+                var fdata = textureData[5];
+
+                for (int y = drawEnd + 1; y < screenDim.Y; y++)
+                {
+                    currentDist = screenDim.Y / (2.0 * y - screenDim.Y);
+                    light = (float)(Light(currentDist));
+
+                    double weight = (currentDist - distPlayer) / (distWall - distPlayer);
+
+                    double currentFloorX = weight * floorXWall + (1.0 - weight) * position.X;
+                    double currentFloorY = weight * floorYWall + (1.0 - weight) * position.Y;
+
+                    var floorTexX = 0;
+                    var floorTexY = 0;
+                    floorTexX = (int)(currentFloorX * tex.Width) % tex.Width;
+                    floorTexY = (int)(currentFloorY * tex.Height) % tex.Height;
+
+                    
+                    var col = new Color(data[tex.Width * floorTexY + floorTexX], light);
+                    buffer[y, x] = col;
+
+                    col = new Color(fdata[tex.Width * floorTexY + floorTexX], light);
+                    buffer[screenDim.Y - y, x] = col;
+                }
+
+            }
+
+            var pixels = new Color[buffer.Length];
+            var p = 0;
+            foreach (var color in buffer)
+                pixels[p++] = color;
+
+            return pixels;
+        }
+
+        private static double Light(double currentDist) => 1.0 / (currentDist);
+    }
+}
---
+++ LD41/Game1.cs
@@ -0,0 +1,216 @@
+﻿using LD41.Raycasting;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LD41
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        const int width = 400;
+        const int height = 300;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D target;
+        int[,] map;
+        Vector2 pos = new Vector2(3, 3);
+        Vector2 look = new Vector2(-1, 0);
+        Vector2 fov = new Vector2(0, 0.66f);
+        Texture2D[] textures;
+        ViewportAdapter va;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Window.AllowUserResizing = true;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            map = new int[11, 5]
+            {
+                { 2, 2, 4, 2, 2 },
+                { 5, 0, 0, 0, 5 },
+                { 5, 0, 0, 0, 5 },
+                { 5, 0, 0, 0, 5 },
+                { 5, 0, 0, 0, 5 },
+                { 5, 0, 0, 0, 5 },
+                { 2, 2, 0, 2, 2 },
+                { 2, 0, 0, 0, 2 },
+                { 2, 0, 0, 0, 2 },
+                { 2, 0, 0, 0, 2 },
+                { 2, 2, 2, 2, 2 },
+            };
+
+            // TODO: Add your initialization logic here
+            //map = new int[24, 24]
+            //{
+            //  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+            //  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+            //  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            //  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+            //};
+
+            base.Initialize();
+
+            va = new ViewportAdapter(Window, GraphicsDevice, width, height);
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            target = new Texture2D(GraphicsDevice, width, height);
+            var floor = Content.Load<Texture2D>("floor");
+            var wall = Content.Load<Texture2D>("wall");
+            var comp = Content.Load<Texture2D>("comp");
+            var window = Content.Load<Texture2D>("window");
+            var ceiling = Content.Load<Texture2D>("ceiling");
+            textures = new Texture2D[]
+            {
+                floor,
+                wall,
+                floor,
+                comp,
+                window,
+                ceiling,
+            };
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var delta = deltaTime(gameTime);
+
+            var kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.Left) && !kb.IsKeyDown(Keys.RightAlt))
+            {
+                var rot = Matrix.Identity * Matrix.CreateRotationZ(0.3f * delta);
+                look = Vector2.Transform(look, rot);
+                fov = Vector2.Transform(fov, rot);
+            }
+            if (kb.IsKeyDown(Keys.Right) && !kb.IsKeyDown(Keys.RightAlt))
+            {
+                var rot = Matrix.Identity * Matrix.CreateRotationZ(-0.3f * delta);
+                look = Vector2.Transform(look, rot);
+                fov = Vector2.Transform(fov, rot);
+            }
+
+            
+            var newPos = pos;
+            if (kb.IsKeyDown(Keys.Up))
+            {
+                newPos += look * delta;
+            }
+            if (kb.IsKeyDown(Keys.Down))
+            {
+                newPos -= look * delta;
+            }
+            if (kb.IsKeyDown(Keys.Left) && kb.IsKeyDown(Keys.RightAlt))
+            {
+                newPos += (new Vector2(-look.Y, look.X) * delta);
+            }
+            if (kb.IsKeyDown(Keys.Right) && kb.IsKeyDown(Keys.RightAlt))
+            {
+                newPos -= (new Vector2(-look.Y, look.X) * delta);
+            }
+
+            var dv = pos - newPos;
+            if (dv == Vector2.Zero) return;
+            var dx = dv.X;
+            var dy = dv.Y;
+
+            var np = new Vector2(newPos.X + (dx > 0 ? -0.6f : dx < 0 ? 0.6f : 0f), pos.Y).ToPoint();
+            if (map[np.X, np.Y] == 0)
+                pos.X = newPos.X;
+
+            np = new Vector2(pos.X, newPos.Y + (dy > 0 ? -0.6f : dy < 0 ? 0.6f : 0f)).ToPoint();
+            if (map[np.X, np.Y] == 0)
+                pos.Y = newPos.Y;
+        }
+
+        private static float deltaTime(GameTime gameTime) =>
+            ((float)gameTime.ElapsedGameTime.TotalMilliseconds / 100f);
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            var data = Raycaster.Raycast(
+                map,
+                new Point(width, height),
+                pos,
+                look,
+                fov,
+                textures);
+
+            target.SetData(data);
+
+            spriteBatch.Begin(blendState: BlendState.NonPremultiplied, transformMatrix: va.GetScaleMatrix());
+            spriteBatch.Draw(target, Vector2.Zero, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
